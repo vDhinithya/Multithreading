9cc375b2632d0a494b36c05c3c25d5757611e88f
vDhinithya <dhinithya12@gmail.com>
2026-03-30 21:15:55 +0530

    interrupt method

--- src/main/java/com/multithreading/MyThread.java
+++ src/main/java/com/multithreading/MyThread.java
@@ -1,52 +1,23 @@
 package com.multithreading;
 
 public class MyThread extends Thread{
-//    @Override
-//    public void run() {
-//
-//
-//            try {
-//                Thread.sleep(5000);
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//
-//        System.out.println("Thread is running ...");
-//
-//    }
-
-    public MyThread (String name){
-        super(name);
-    }
 
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++) {
-            String a = "";
-            for (int j = 0; j < 10000; j++) {
-                a += "a";
-            }
-            System.out.println(Thread.currentThread().getName() + " - Priority: " + Thread.currentThread().getPriority() + " - count:" + i);
-            try {
-                Thread.sleep(100);
-            } catch (Exception e) {
-
-            }
+        try {
+            Thread.sleep(1000);
+            System.out.println("Thread is running...");
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted:" + e);
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        MyThread l = new MyThread("Low Priority");
-        MyThread m = new MyThread("Mid priority");
-        MyThread h = new MyThread("High Priority");
-        l.setPriority(Thread.MIN_PRIORITY);
-        m.setPriority(Thread.NORM_PRIORITY);
-        h.setPriority(Thread.MAX_PRIORITY);
-        l.start();
-        m.start();
-        h.start();
-        
+
+        MyThread t1 = new MyThread();
+        t1.start();
+        t1.interrupt(); // this says stop the current working thread in its state
     }
 }
 
-// start run sleep join setPriority
+// start run sleep join setPriority interrupt
